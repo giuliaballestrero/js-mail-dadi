35a5c43053c97cb6f307f48a024a56badffe55bf
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-24 15:01:19 +0100

    Add in ex1- Dice Game third else if (match drawn)

--- js/script.js
+++ js/script.js
@@ -3,24 +3,19 @@
  * 1. Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-    2. Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere.
-Stampa un messaggio appropriato sull’esito del controllo.
 */
 
 // creo la lista di numeri da 1 a 6 
-
 const numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6'
 ]
 
-console.log (numbers)
+console.log (numbers);
 
 // estraggo un numero random per il giocatore 1
 
@@ -33,17 +28,34 @@ const randomNumber2 = numbers[Math.floor(Math.random() * numbers.length)];
 console.log(randomNumber2);
 
 //stabilisco il vincitore in base al numero più alto 
-if (randomNumber1 > randomNumber2){
+if (randomNumber1 > randomNumber2) {
     console.log('Giocatore 1, hai vinto!');
 }
 
- else if (randomNumber2 > randomNumber1){
+ else if (randomNumber2 > randomNumber1) {
     console.log('Giocatore 2, hai vinto!');
  }
-    
-    
-    
+
+ else if (randomNumber2 == randomNumber1) 
+    console.log('Pareggio, rilanciare il dado!');
 
 
-    
+/**
+ *     2. Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere.
+Stampa un messaggio appropriato sull’esito del controllo.
 
+
+//creo una lista di email 
+
+const mailList = [
+    'chandlerbing@friends.com',
+    'rachelgreen@friends.com',
+    'phoebebuffay@friends.com',
+    'monicageller@friends.com',
+    'joeytribbiani@friends.com',
+    'rossgeller@friends.com'
+]
+
+console.log (mailList)
+ */
